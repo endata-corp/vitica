19880b817461e54c6358c1b93a415be6ffaea7fd
Ricardo Rossi <rrossi@endata.com>
2015-06-18 19:22:57 -0500

    Added price filtering

--- data/products.go
+++ data/products.go
@@ -1,6 +1,9 @@
 package data
 
-import "vitica/log"
+import (
+	"strings"
+	"vitica/log"
+)
 
 type CategoryOptions struct {
 	Path   string
@@ -12,8 +15,13 @@ func GetProducts(options CategoryOptions) (err error, products []Product) {
 	log.Info("%+v ", options)
 	products = []Product{}
 	db := DB()
-	err = db.Where(getFieldNameFromPath(options.Path) + " = 1").
-		Find(&products).Error
+	db = db.Where(getFieldNameFromPath(options.Path) + " = 1")
+	if options.Price != "" && options.Price != "any" {
+		prices := strings.Split(options.Price, "-")
+		db = db.Where("price >= " + prices[0])
+		db = db.Where("price <= " + prices[1])
+	}
+	err = db.Find(&products).Error
 	return
 }
 
